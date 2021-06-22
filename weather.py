14cdaeca55d1bedba99d1f51f05704151b91c984
Anandhu1228 <84179857+Anandhu1228@users.noreply.github.com>
2021-06-23 01:41:48 +0530

    uploaded the project file

---
+++ weather.py
@@ -0,0 +1,27 @@
+import requests
+
+from datetime import datetime
+
+api_key = '226b21ff1f43a1dbe3cd06fc766b6352'
+location = input("ENTER THE CITY NAME :   ")
+
+complete_api_link = "https://api.openweathermap.org/data/2.5/weather?q="+location+"&appid="+api_key
+api_link = requests.get(complete_api_link)
+api_data = api_link.json()
+
+temp_city = ((api_data['main']['temp']) - 273.15)
+weather_desc = api_data['weather'][0]['description']
+hmdt = api_data['main']['humidity']
+wind_spd = api_data['wind']['speed']
+date_time = datatime.now().strftime("%d %b %y | %I:%M:%S %p")
+
+
+
+print("-------------------------------------------------------------")
+print("Weather stats for - {}  ||  {}".format(location.upper(),date_time))
+print("-------------------------------------------------------------")
+
+print("current temperature is : {:.2f} deg C".format(temp_city))
+print("current weather desc   :",weather_desc)
+print("current humidity       :",hmdt, '%')
+print("current wind speed     :",wind_spd,'kmph')
